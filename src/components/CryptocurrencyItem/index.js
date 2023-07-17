cef9fe3e390016bd90179425c41f401ac03ad51f
ashushikhare9394 <shikhareashwini9394@gmail.com>
2023-07-17 09:51:40 +0000

    commit message

--- src/components/CryptocurrencyItem/index.js
+++ src/components/CryptocurrencyItem/index.js
@@ -3,13 +3,13 @@ import './index.css'
 
 const CryptocurrencyItem = props => {
   const {currencyDetails} = props
-  const {currencyName, usdValue, euroValue, currencyLogo} = currencyDetails
+  const {currencyName, usdValue, euroValue, currencyLogo, id} = currencyDetails
 
   return (
     <li>
       <div className="blogItem">
         <img src={currencyLogo} alt={currencyName} className="currencyLogo" />
-        <h1 className="name">{currencyName}</h1>
+        <p className="name">{currencyName}</p>
         <p className="name">{usdValue}</p>
         <p className="name">{euroValue}</p>
       </div>
--- src/components/CryptocurrencyTracker/index.js
+++ src/components/CryptocurrencyTracker/index.js
@@ -3,10 +3,10 @@ import CryptocurrenciesList from '../CryptocurrenciesList'
 
 import './index.css'
 
-const CryptocurrencyTracker = () => {
-  ;<div className="bg-container">
+const CryptocurrencyTracker = () => (
+  <div className="bg-container">
     <CryptocurrenciesList />
   </div>
-}
+)
 
 export default CryptocurrencyTracker
